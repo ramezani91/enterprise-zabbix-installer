d00feb84f654d446255df996336fd97e785c0440
Hamed Ramezani <ramezani91.e@gmail.com>
2026-07-10 00:44:07 +0330

    Add common library loader

---
+++ lib/common.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+########################################
+# Enterprise Zabbix Common Loader
+########################################
+
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+export BASE_DIR
+
+
+source "$BASE_DIR/lib/colors.sh"
+source "$BASE_DIR/lib/logger.sh"
+source "$BASE_DIR/lib/config.sh"
+source "$BASE_DIR/lib/utils.sh"
+source "$BASE_DIR/lib/validator.sh"
+
+
+info "Enterprise Zabbix libraries loaded"
